5ca007f30d181ca63a015823ab1a3d3179c2b780
s4chzeye <s4chzeye@users.noreply.github.com>
2015-01-06 19:38:11 +0100

    added junit test class

---
+++ FST_ueb4/src/fst/list/LinkedListTest.java
@@ -0,0 +1,19 @@
+package fst.list;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	@Test
+	public void testCreateList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+}
